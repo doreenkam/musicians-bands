39d28ce1ed4db5c3e83571a792a3401f7a4f9558
Doreen Kamushinda <dkamushinda@expediagroup.com>
2023-02-15 23:43:56 +0000

    Completed part 1

--- Musician.js
+++ Musician.js
@@ -1,8 +1,13 @@
-const {Sequelize, sequelize} = require('./db');
+const { Sequelize, sequelize } = require('./db');
 
 // TODO - define the Musician model
-let Musician;
+let Musician = sequelize.define('musician', {
+  name: Sequelize.STRING,
+  albums: Sequelize.INTEGER,
+  genre: Sequelize.STRING,
+  country: Sequelize.STRING,
+});
 
 module.exports = {
-    Musician
+  Musician,
 };
--- index.test.js
+++ index.test.js
@@ -1,24 +1,37 @@
-const {sequelize} = require('./db');
-const {Band, Musician} = require('./index')
+const { sequelize } = require('./db');
+const { Band, Musician } = require('./index');
 
 describe('Band and Musician Models', () => {
-    /**
-     * Runs the code prior to all tests
-     */
-    beforeAll(async () => {
-        // the 'sync' method will create tables based on the model class
-        // by setting 'force:true' the tables are recreated each time the 
-        // test suite is run
-        await sequelize.sync({ force: true });
-    })
+  /**
+   * Runs the code prior to all tests
+   */
+  beforeAll(async () => {
+    // the 'sync' method will create tables based on the model class
+    // by setting 'force:true' the tables are recreated each time the
+    // test suite is run
+    await sequelize.sync({ force: true });
+  });
 
-    test('can create a Band', async () => {
-        // TODO - test creating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+  test('can create a Band', async () => {
+    // TODO - test creating a band
+    let band1 = await Band.create({
+      name: 'The Beatles',
+      members: 4,
+      albums: 12,
+      genre: 'Rock',
+    });
+    expect(band1.members).toBe(4);
+    expect(band1.genre).toBe('Rock');
+  });
 
-    test('can create a Musician', async () => {
-        // TODO - test creating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
-})
+  test('can create a Musician', async () => {
+    // TODO - test creating a musician
+    let musician1 = await Musician.create({
+      name: 'Rihanna',
+      albums: 8,
+      genre: 'Pop / R&B',
+      country: 'Barbados',
+    });
+    expect(musician1.name).toBe('Rihanna');
+  });
+});
